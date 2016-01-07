9ea1ebe65e60d68fc85a013472c3d904e069ea39
gwangjin <wgj2050@naver.com>
2016-01-07 15:22:22 +0900

    Create exprlinkedstack.h

---
+++ exprlinkedstack.h
@@ -0,0 +1,35 @@
+#pragma once
+#pragma once
+#ifndef _LINKED_STACK_
+#define _LINKED_STACK_
+
+
+typedef struct StackNodeType
+{
+	ExprToken data;
+	struct StackNodeType* pLink;
+} StackNode;
+
+typedef struct LinkedStackType
+{
+	int currentElemnetCount;
+	StackNode* pTopElement;
+} LinkedStack;
+
+LinkedStack* createLinkedStack();
+int pushLS(LinkedStack* pStack, StackNode element);
+StackNode* popLS(LinkedStack* pStack);
+StackNode* peekLS(LinkedStack* pStack);
+void deleteLinkedStack(LinkedStack* pStack);
+int isLinkedStackFull(LinkedStack* pStack);
+int isLinkedStackEmpty(LinkedStack* pStack);
+
+#endif
+
+#ifndef _COMMON_STACK_DEF_
+#define _COOMON_STACK_DEF_
+
+#define TRUE 1
+#define FALSE 0
+
+#endif
